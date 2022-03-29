21fb29da757ac02193fe08ee6429a299521385bc
balagopalg <balagopal93@gmail.com>
2022-03-29 09:41:35 +0800

    Remove credentials from env-constants

--- backend/src/utils/env-constants.ts
+++ backend/src/utils/env-constants.ts
@@ -3,8 +3,8 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 
 export const mySQLDatabase = process.env.mySQLDatabase ? process.env.mySQLDatabase : 'carsome'
-export const mySQLPassword = process.env.mySQLPassword ? process.env.mySQLPassword : 'carsome-new'
-export const mySQLUser = process.env.mySQLUser ? process.env.mySQLUser : 'admin'
-export const mySQLHost = process.env.mySQLHost ? process.env.mySQLHost : 'carsome-new.cjqzcldwdmgi.ap-southeast-1.rds.amazonaws.com'
+export const mySQLPassword = process.env.mySQLPassword ? process.env.mySQLPassword : ''
+export const mySQLUser = process.env.mySQLUser ? process.env.mySQLUser : ''
+export const mySQLHost = process.env.mySQLHost ? process.env.mySQLHost : ''
 export const MYSQLPORT = process.env.PORT ? process.env.PORT : '3306'
 export const PORT = process.env.PORT ? process.env.PORT : '5000'
